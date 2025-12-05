e2de2b3238b6d848bf6502f5653d319896b40921
Jesse Koivukoski <jesse.koivukoski@taitounited.fi>
2025-12-05 14:51:50 +0200

    Optimize day 4

--- README.md
+++ README.md
@@ -12,10 +12,10 @@
 | [Day 1](./src/bin/01.rs) | `72.8µs` | `76.1µs` |
 | [Day 2](./src/bin/02.rs) | `2.6µs` | `14.1µs` |
 | [Day 3](./src/bin/03.rs) | `35.0µs` | `117.7µs` |
-| [Day 4](./src/bin/04.rs) | `153.6µs` | `1.3ms` |
+| [Day 4](./src/bin/04.rs) | `111.2µs` | `261.0µs` |
 | [Day 5](./src/bin/05.rs) | `49.4µs` | `31.0µs` |
 
-**Total: 1.85ms**
+**Total: 0.77ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,4 +1,7 @@
-use advent_of_code::utils::{grid::Grid, point::Point};
+use advent_of_code::utils::{
+    grid::Grid,
+    point::{DIAGONAL, Point},
+};
 
 advent_of_code::solution!(4);
 
@@ -6,93 +9,88 @@ fn parse_input(input: &str) -> Grid<u8> {
     Grid::parse(input)
 }
 
-fn is_accessible(grid: &Grid<u8>, point: Point) -> bool {
-    if grid[point] != b'@' {
-        return false;
-    }
-
-    let mut count = 0;
-
-    // Loop through orthogonal + diagonal neighbors
-    for y_n in (point.y - 1).max(0)..=(point.y + 1).min(grid.height - 1) {
-        for x_n in (point.x - 1).max(0)..=(point.x + 1).min(grid.width - 1) {
-            let neighbor = Point::new(x_n, y_n);
-
-            if point == neighbor {
-                continue;
-            }
-
-            if grid[neighbor] == b'@' {
-                count += 1;
-            }
-        }
-    }
-
-    count < 4
-}
-
 pub fn part_one(input: &str) -> Option<u64> {
     let grid = parse_input(input);
-    let mut accessibles = 0;
+    let mut total = 0;
 
     for y in 0..grid.height {
         for x in 0..grid.width {
             let point = Point::new(x, y);
 
-            if is_accessible(&grid, point) {
-                accessibles += 1;
+            if grid[point] != b'@' {
+                continue;
+            }
+
+            let count = DIAGONAL
+                .iter()
+                .filter(|d| {
+                    let n = point + **d;
+                    n.x >= 0 && n.y >= 0 && n.x < grid.width && n.y < grid.height && grid[n] == b'@'
+                })
+                .count();
+
+            if count < 4 {
+                total += 1;
             }
         }
     }
 
-    Some(accessibles as u64)
-}
-
-fn attempt_remove(grid: &mut Grid<u8>, point: Point) -> bool {
-    if is_accessible(grid, point) {
-        grid[point] = b'.';
-        return true;
-    }
-
-    false
+    Some(total)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let mut grid = parse_input(input);
-    let mut touched;
-    let mut count = 0;
+    let grid = parse_input(input);
+    // New grid that holds the number of neighboring rolls
+    // for each roll cell
+    let mut count_grid = Grid::new(grid.width, grid.height, i32::MAX);
+    let mut todo = vec![];
 
-    loop {
-        touched = false;
+    for y in 0..grid.height {
+        for x in 0..grid.width {
+            let point = Point::new(x, y);
 
-        for y in 0..grid.height {
-            for x in 0..grid.width {
-                let point = Point::new(x, y);
+            if grid[point] != b'@' {
+                continue;
+            }
 
-                if attempt_remove(&mut grid, point) {
-                    count += 1;
-                    touched = true;
+            let count = DIAGONAL
+                .iter()
+                .filter(|d| {
+                    let n = point + **d;
+                    n.x >= 0 && n.y >= 0 && n.x < grid.width && n.y < grid.height && grid[n] == b'@'
+                })
+                .count();
 
-                    // As an optimization, re-check previous neighbors
-                    // (2x speed up)
-                    count += [
-                        Point::new((x - 1).max(0), y),
-                        Point::new(x, (y - 1).max(0)),
-                        Point::new((x - 1).max(0), (y - 1).max(0)),
-                        Point::new((x + 1).min(grid.width - 1), (y - 1).max(0)),
-                    ]
-                    .iter()
-                    .filter(|n| attempt_remove(&mut grid, **n))
-                    .count();
-                }
+            count_grid[point] = count as i32;
+
+            if count < 4 {
+                todo.push(point);
             }
         }
+    }
 
-        if !touched {
-            break;
+    let mut total = 0;
+
+    // Start deleting rolls
+    while let Some(point) = todo.pop() {
+        total += 1;
+
+        for neighbor in DIAGONAL
+            .into_iter()
+            .map(|d| point + d)
+            .filter(|n| n.x >= 0 && n.y >= 0 && n.x < grid.width && n.y < grid.height)
+        {
+            // If a roll causes a neighbor roll to become
+            // accessible, add that to todo
+            if count_grid[neighbor] == 4 {
+                todo.push(neighbor);
+            }
+
+            count_grid[neighbor] -= 1;
         }
     }
-    Some(count as u64)
+
+    Some(total)
 }
 
 #[cfg(test)]
